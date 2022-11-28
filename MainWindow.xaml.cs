dd758deabe57456de081be69b29cde75ddfc73e5
nebeesna <75483446+nebeesna@users.noreply.github.com>
2022-11-28 15:16:20 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace hotPlans
 {
     /// <summary>
@@ -23,7 +26,27 @@ namespace hotPlans
         public MainWindow()
         {
             InitializeComponent();
+            //DataContext = this;
+
+            // Need to initialize this, otherwise you get a null exception
+            //Tasks = new ObservableCollection<string>();            
+            
         }
+
+
+        //public ListView Tasks { get; set; }
+        //public ObservableCollection<string> Tasks { get; set; }
+
+        // Add an item to the list        
+        //private void Button_Click_Add(object sender, RoutedEventArgs e)
+        //{
+        //    // Custom control for entering a single string
+        //    SingleEntryDialog _Dlg = new SingleEntryDialog();
+
+        //    // OutputBox is a string property of the custom control
+        //    if ((bool)_Dlg.ShowDialog())
+        //        Tasks.Add(_Dlg.OutputBox.Trim());
+        //}
 
     }
 }
